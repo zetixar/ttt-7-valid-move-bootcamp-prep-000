b58ee67b8e4e9f5190a5376ee6348fd09671c199
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 17:49:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? board, index
-  if index != 0.between(8) or position_taken? board, index
+  if index != index.between(0, 8) or position_taken? board, index
     return false
   else
     return true
